10a56dddea55e2d540b2178a53f06f6bc1786c55
yeongun130 <yeongunj625@gmail.com>
2023-06-26 21:04:08 +0900

    ComposeUI LazeColumn

--- app/src/main/java/com/composebasic/ui/theme/Type.kt
+++ app/src/main/java/com/composebasic/ui/theme/Type.kt
@@ -4,6 +4,7 @@ import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
@@ -12,6 +13,11 @@ val Typography = Typography(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
     )
     /* Other default text styles to override
     button = TextStyle(
