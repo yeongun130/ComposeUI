38803378a5948689464559eb2fb22aebd3dfe1a5
yeongun130 <yeongunj625@gmail.com>
2023-10-25 18:19:27 +0900

    ComposeUI - Pager

---
+++ app/src/main/java/com/composebasic/ComposeUIPager.kt
@@ -0,0 +1,16 @@
+package com.composebasic
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun Pager() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PagerPreview() {
+
+}
