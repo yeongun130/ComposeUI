6284682c6668d7b296b0ceb20ccd785c9bac29e2
정영운 <108732289+yeongun130@users.noreply.github.com>
2024-01-13 09:42:19 +0900

    Update ComposeUIPager.kt

--- app/src/main/java/com/composebasic/ComposeUIPager.kt
+++ app/src/main/java/com/composebasic/ComposeUIPager.kt
@@ -6,11 +6,50 @@ import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun Pager() {
+    HorizontalPager(
+        state = pagerState
+    ) { page ->
+        when (page % 3) {
+              0 -> {
+                  Image(
+                      painter = painterResource(R.drawable.first_banner),
+                      contentDescription = "First Banner",
+                      modifier = modifier.fillMaxWidth(),
+                      contentScale = ContentScale.FillBounds
+                  )
+            }
 
+            1 -> {
+                Image(
+                    painter = painterResource(R.drawable.second_banner),
+                    contentDescription = "Second Banner",
+                    modifier = modifier.fillMaxWidth(),
+                    contentScale = ContentScale.FillBounds
+                )
+            }
+
+            2 -> {
+                Image(
+                    painter = painterResource(R.drawable.third_banner),
+                    contentDescription = "Third Banner",
+                    modifier = modifier.fillMaxWidth(),
+                    contentScale = ContentScale.FillBounds
+                )
+            }
+        }
+    }
+    Column(
+        modifier = modifier.padding(bottom = 12.dp)
+    ) {
+        PagerIndicator(
+                count = 3,
+                currentPage = pagerState.currentPage % 3,
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun PagerPreview() {
-
+      Pager()
 }
